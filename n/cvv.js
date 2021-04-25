9ab3f7165c18376614dff79c02c3de415b84ccee
SirijaKasi <76515131+SirijaKasi@users.noreply.github.com>
2021-04-25 19:03:01 +0530

    Add files via upload

---
+++ n/cvv.js
@@ -0,0 +1,4 @@
+ cool();
+                function cool(){
+                document.write("welcome!!......");
+                           }
---
+++ n/m.js
@@ -0,0 +1,19 @@
+
+
+// function app(){
+// 	document.getElementById("p").innerHTML = "Paragraph changed.";
+// }
+
+function cool(){
+	var n=40;
+	if (n<60) {
+		document.write("A grade");
+	}
+	else(){
+		document.write("b grade");
+	}
+}
+
+cool();
+
+
