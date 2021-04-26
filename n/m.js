0505ae52b0a99779b3c302bd3df1f5d4a3c8659d
Sirija Kasi <sirijagowd00@gmail.com>
2021-04-26 15:34:59 +0530

    update

--- n/m.js
+++ n/m.js
@@ -6,15 +6,26 @@ function cool() {
   var n = 40;
   if (n < 60) {
     document.write("A grade");
-  } else
-    () => {
-      document.write("b grade");
-    };
+  } else {
+    document.write("b grade");
+  }
 }
 cool();
 
-function BrowserOM() {
-  var x = prompt("enter name");
-  document.write(x); // prints the data on the webpage
+// function BrowserOM() {
+//   // open("www.youtube.com");
+//   var x = prompt("enter name");
+//   document.write(x); // prints the data on the webpage
+// }
+// BrowserOM();
+
+// function open() {
+//   open("https://www.google.com");
+// }
+
+function gobackPage() {
+  history.back(); // webpage go back button
 }
-BrowserOM();
+gobackPage();
+
+
